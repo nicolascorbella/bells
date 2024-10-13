254e295bdf0644c91af280ef9c35f2a61ceccbc3
Nicolas Corbella <112200488+nicolascorbella@users.noreply.github.com>
2024-10-13 20:25:07 -0300

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,39 +1,37 @@
+// Importaciones necesarias
 import express from "express";
 import cors from "cors";
-import path from "path"; // Para manejar rutas de archivos
-import { fileURLToPath } from "url"; // Para obtener el directorio actual
-import nodemailer from "nodemailer"; // Importar Nodemailer
-
-// SDK de Mercado Pago
+import path from "path";
+import { fileURLToPath } from "url";
+import nodemailer from "nodemailer";
 import { MercadoPagoConfig, Preference } from "mercadopago";
 
-// Agrega credenciales de Mercado Pago
+// Configuración de Mercado Pago
 const client = new MercadoPagoConfig({
   accessToken: "APP_USR-1935091980734919-092313-dac02186b864d3a6e68be45881369cb0-229579824",
 });
 
-// Obtener el directorio actual en ESM (equivalente a __dirname)
+// Configuración de __dirname en ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Inicialización de la app de Express
 const app = express();
 
-// Middleware para permitir CORS y parsear JSON
-app.use(cors({
-  origin: "https://www.theguardianbell.com.ar", // Permitir solo tu dominio
-}));
+// Middlewares
+app.use(cors({ origin: "https://www.theguardianbell.com.ar" }));
 app.use(express.json());
 
 // Configuración de Nodemailer
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "nicolascorbellaortiz@gmail.com", // Tu correo Gmail
+    user: "nicolascorbellaortiz@gmail.com",
     pass: "gpxt pdaz cdxj mbqb", // Contraseña de aplicación
   },
 });
 
-// Sirve archivos estáticos desde la carpeta 'public'
+// Configuración para servir archivos estáticos
 app.use(express.static(path.join(__dirname, './public'), {
   setHeaders: (res, path) => {
     if (path.endsWith('.webp')) {
@@ -42,24 +40,21 @@ app.use(express.static(path.join(__dirname, './public'), {
   }
 }));
 
-// Ruta para servir el archivo index.html cuando se accede a la raíz "/"
+// Rutas para servir archivos HTML
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html")); // Esto carga el 'index.html' desde 'public'
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Ruta para la tienda
 app.get("/tienda", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "tienda.html")); // Ruta directa al archivo tienda.html
+  res.sendFile(path.join(__dirname, "public", "tienda.html"));
 });
 
-// Ruta para el carrito
 app.get("/carrito", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "carrito.html")); // Ruta directa al archivo carrito.html
+  res.sendFile(path.join(__dirname, "public", "carrito.html"));
 });
 
-// Ruta para la calavera
 app.get("/calavera", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "campanas", "calavera", "index.html")); // Ruta directa al archivo calavera.html
+  res.sendFile(path.join(__dirname, "public", "campanas", "calavera", "index.html"));
 });
 
 // Ruta para crear una preferencia de pago en Mercado Pago
@@ -85,78 +80,53 @@ app.post("/create_preference", async (req, res) => {
     const preference = new Preference(client);
     const result = await preference.create({ body });
 
-    res.json({
-      id: result.id,
-    });
+    res.json({ id: result.id });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      error: "Error al crear la preferencia :(",
-    });
+    console.error("Error al crear la preferencia:", error);
+    res.status(500).json({ error: "Error al crear la preferencia :(" });
   }
 });
 
-// Nueva ruta para enviar el correo con los detalles de la compra
+// Ruta para enviar correo electrónico con los detalles de la compra
 app.post("/enviar_correo", (req, res) => {
-  const { nombre, metodoEntrega, localidad, calle, horario } = req.body;
+  const { nombre, metodoEntrega, localidad, calle, telefono, price } = req.body;
 
-  // Configurar el mensaje según la información enviada
-const mailOptions = {
-  from: "nicolascorbellaortiz@gmail.com",
-  to: "nicolascorbellaortiz@gmail.com",
-  subject: "Nueva compra confirmada",
-  html: `
-    <html>
-      <head>
-        <style>
-          body {
-            font-family: Arial, sans-serif;
-            color: #333;
-          }
-          h3 {
-            color: #0056b3;
-          }
-          ul {
-            list-style: none;
-            padding: 0;
-          }
-          li {
-            margin-bottom: 10px;
-            padding: 8px;
-            background-color: #f9f9f9;
-            border: 1px solid #ddd;
-            border-radius: 4px;
-          }
-          li strong {
-            color: #555;
-          }
-          .price {
-            font-size: 1.2em;
-            font-weight: bold;
-            color: #007b00;
-          }
-        </style>
-      </head>
-      <body>
-        <h3>Detalles de la compra:</h3>
-        <ul>
-          <li><strong>Nombre:</strong> ${nombre || "No especificado"}</li>
-          <li><strong>Método de entrega:</strong> ${metodoEntrega || "No especificado"}</li>
-          ${metodoEntrega === "envio" ? `
-            <li><strong>Localidad:</strong> ${localidad || "No especificado"}</li>
-            <li><strong>Calle:</strong> ${calle || "No especificado"}</li>
-            <li><strong>Número de teléfono:</strong> ${telefono || "No especificado"}</li>
-          ` : ""}
-          <li class="price"><strong>Precio:</strong> ${parseFloat(req.body.price).toFixed(2) || "No especificado"} $ ARS</li>
-        </ul>
-      </body>
-    </html>
-  `,
-};
-  // Enviar el correo
+  const mailOptions = {
+    from: "nicolascorbellaortiz@gmail.com",
+    to: "nicolascorbellaortiz@gmail.com",
+    subject: "Nueva compra confirmada",
+    html: `
+      <html>
+        <head>
+          <style>
+            body { font-family: Arial, sans-serif; color: #333; }
+            h3 { color: #0056b3; }
+            ul { list-style: none; padding: 0; }
+            li { margin-bottom: 10px; padding: 8px; background-color: #f9f9f9; border: 1px solid #ddd; border-radius: 4px; }
+            li strong { color: #555; }
+            .price { font-size: 1.2em; font-weight: bold; color: #007b00; }
+          </style>
+        </head>
+        <body>
+          <h3>Detalles de la compra:</h3>
+          <ul>
+            <li><strong>Nombre:</strong> ${nombre || "No especificado"}</li>
+            <li><strong>Método de entrega:</strong> ${metodoEntrega || "No especificado"}</li>
+            ${metodoEntrega === "envio" ? `
+              <li><strong>Localidad:</strong> ${localidad || "No especificado"}</li>
+              <li><strong>Calle:</strong> ${calle || "No especificado"}</li>
+              <li><strong>Número de teléfono:</strong> ${telefono || "No especificado"}</li>
+            ` : ""}
+            <li class="price"><strong>Precio:</strong> ${parseFloat(price).toFixed(2) || "No especificado"} $ ARS</li>
+          </ul>
+        </body>
+      </html>
+    `,
+  };
+
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log("Error al enviar el correo:", error);
+      console.error("Error al enviar el correo:", error);
       res.status(500).json({ success: false, error: "Error al enviar el correo" });
     } else {
       console.log("Correo enviado: " + info.response);
@@ -165,12 +135,11 @@ const mailOptions = {
   });
 });
 
-// Iniciar el servidor en un puerto específico
+// Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 
-// Exporta el servidor de Express para que funcione como una serverless function en Vercel
+// Exportar app
 export default app;
-
